db9cd6592044bdfe70fdd60e49e46be9cd60f89d
Shreyas Zare <shreyas@technitium.com>
2026-06-27 12:02:57 +0530

    DnsApplicationManager: Updated InstallApplicationAsync() to use async methods and to dispose zip file on execption to allow deleting the app folder. Updated UpdateApplicationAsync() to use async methods, ensure that the extracted file is inside the destination folder, and handle directory entry case. Updated DownloadAndInstallAppAsync() and DownloadAndUpdateAppAsync() to implement idle timeout mechanism.

--- DnsServerCore/Dns/Applications/DnsApplicationManager.cs
+++ DnsServerCore/Dns/Applications/DnsApplicationManager.cs
@@ -29,6 +29,7 @@ using System.Text;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
+using TechnitiumLibrary.IO;
 using TechnitiumLibrary.Net.Http.Client;
 
 namespace DnsServerCore.Dns.Applications
@@ -387,16 +388,18 @@ namespace DnsServerCore.Dns.Applications
 
                 zipCopyStream.Position = 0;
 
-                using (ZipArchive appZip = new ZipArchive(zipCopyStream, ZipArchiveMode.Read, false, Encoding.UTF8))
+                await using (ZipArchive appZip = new ZipArchive(zipCopyStream, ZipArchiveMode.Read, false, Encoding.UTF8))
                 {
                     try
                     {
-                        appZip.ExtractToDirectory(applicationFolder, true);
+                        await appZip.ExtractToDirectoryAsync(applicationFolder, true);
 
                         return await LoadApplicationAsync(applicationFolder, true);
                     }
                     catch
                     {
+                        await appZip.DisposeAsync();
+
                         if (Directory.Exists(applicationFolder))
                             Directory.Delete(applicationFolder, true);
 
@@ -420,25 +423,31 @@ namespace DnsServerCore.Dns.Applications
 
                 zipCopyStream.Position = 0;
 
-                using (ZipArchive appZip = new ZipArchive(zipCopyStream, ZipArchiveMode.Read, false, Encoding.UTF8))
+                await using (ZipArchive appZip = new ZipArchive(zipCopyStream, ZipArchiveMode.Read, false, Encoding.UTF8))
                 {
                     UnloadApplication(applicationName);
 
                     foreach (ZipArchiveEntry entry in appZip.Entries)
                     {
-                        string entryPath = entry.FullName;
-
-                        if (Path.DirectorySeparatorChar != '/')
-                            entryPath = entryPath.Replace('/', '\\');
-
-                        string filePath = Path.Combine(applicationFolder, entryPath);
+                        string filePath = Path.GetFullPath(Path.Combine(applicationFolder, entry.FullName));
+                        if (!filePath.StartsWith(applicationFolder + Path.DirectorySeparatorChar))
+                            throw new IOException("Extracting Zip entry would have resulted in a file outside the specified destination directory.");
 
                         if ((entry.Name == "dnsApp.config") && File.Exists(filePath))
                             continue; //avoid overwriting existing config file
 
-                        Directory.CreateDirectory(Path.GetDirectoryName(filePath));
+                        if ((entry.Length == 0) && (entry.Name.Length == 0) && entry.FullName.EndsWith('/'))
+                        {
+                            //directory entry
+                            Directory.CreateDirectory(filePath);
+                        }
+                        else
+                        {
+                            //file entry
+                            Directory.CreateDirectory(Path.GetDirectoryName(filePath));
 
-                        entry.ExtractToFile(filePath, true);
+                            await entry.ExtractToFileAsync(filePath, true);
+                        }
                     }
 
                     return await LoadApplicationAsync(applicationFolder, true);
@@ -484,9 +493,14 @@ namespace DnsServerCore.Dns.Applications
 
                     using (HttpClient http = new HttpClient(handler))
                     {
-                        await using (Stream httpStream = await http.GetStreamAsync(uri))
+                        HttpResponseMessage httpResponse = await http.GetAsync(uri, HttpCompletionOption.ResponseHeadersRead);
+
+                        httpResponse.EnsureSuccessStatusCode();
+
+                        await using (Stream httpStream = await httpResponse.Content.ReadAsStreamAsync())
                         {
-                            await httpStream.CopyToAsync(fS);
+                            //copy stream with idle timeout
+                            await httpStream.CopyToAsync(fS, TimeSpan.FromSeconds(60));
                         }
                     }
 
@@ -523,9 +537,14 @@ namespace DnsServerCore.Dns.Applications
 
                     using (HttpClient http = new HttpClient(handler))
                     {
-                        await using (Stream httpStream = await http.GetStreamAsync(uri))
+                        HttpResponseMessage httpResponse = await http.GetAsync(uri, HttpCompletionOption.ResponseHeadersRead);
+                        
+                        httpResponse.EnsureSuccessStatusCode();
+
+                        await using (Stream httpStream = await httpResponse.Content.ReadAsStreamAsync())
                         {
-                            await httpStream.CopyToAsync(fS);
+                            //copy stream with idle timeout
+                            await httpStream.CopyToAsync(fS, TimeSpan.FromSeconds(60));
                         }
                     }
 
